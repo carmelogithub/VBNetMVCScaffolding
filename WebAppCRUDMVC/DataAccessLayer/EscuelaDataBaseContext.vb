95dbd1f703a30b657658bdeccc97c3ee3fffc315
carmelogithub <carmelo.escribano@gmail.com>
2021-09-13 15:26:24 +0200

    creacion context

---
+++ WebAppCRUDMVC/DataAccessLayer/EscuelaDataBaseContext.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Data.Entity
+
+Public Class EscuelaDataBaseContext
+    Inherits DbContext
+
+    Public Sub New()
+        MyBase.New("EscuelaContext")
+    End Sub
+
+    Public Property Estudiantes As DbSet(Of Estudiante)
+
+End Class
