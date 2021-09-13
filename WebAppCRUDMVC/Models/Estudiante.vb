b091c9bcf10893d7189a3f7ccbbea696a3dc7624
carmelogithub <carmelo.escribano@gmail.com>
2021-09-13 15:19:23 +0200

    creación de modelo

---
+++ WebAppCRUDMVC/Models/Estudiante.vb
@@ -0,0 +1,25 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
+
+
+Public Class Estudiante
+
+    <Key>
+    <DatabaseGenerated(DatabaseGeneratedOption.Identity)>
+    Public Property ID As Integer
+    Public Property Nombre As String
+    Public Property Apellido As String
+    Public Property Curso As Curso
+    Public Property FechaInicio As DateTime
+
+
+End Class
+
+Public Enum Curso
+    DAM
+    DAW
+    SMIR
+    ASIR
+End Enum
+
+
